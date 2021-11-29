3d6112a4b5a6863d64a40bb8d33432ec2c7fe6b5
abeltyr <abeltyr@gmail.com>
2021-11-29 14:30:30 +0300

    (feat) update on the _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,33 +1,28 @@
-// import React from 'react';
-// import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-// class MyDocument extends Document {
-//     render() {
-//         return (
-//             <Html lang='en-US'>
-//                 <Head>
-//                     <link
-//                         href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'
-//                         rel='stylesheet'
-//                     />
-//                 </Head>
-//                 <body>
-//                     <Main />
-//                     <NextScript />
-//                 </body>
-//             </Html>
-//         );
-//     }
-// }
-
-// export default MyDocument;
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 
 
-import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang='en-US'>
+                <Head>
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'
+                        rel='stylesheet'
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+
     static async getInitialProps(ctx: any) {
         const sheet = new ServerStyleSheet()
         const originalRenderPage = ctx.renderPage
