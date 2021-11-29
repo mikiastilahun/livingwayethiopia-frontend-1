290089f2e6a78833d88b776d18a11fcc00eb1b66
abeltyr <abeltyr@gmail.com>
2021-11-29 14:23:42 +0300

    (fix) style component fix#1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,40 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { NavBarProvider } from '../contexts/navbar/index'
 import { DrawerProvider } from '../contexts/drawer'
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from '../styles/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    scroll-behavior: smooth;
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
 
-  return <NavBarProvider>
-    <Component {...pageProps} />
-  </NavBarProvider>
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+
+  return <>
+    <GlobalStyle />
+
+    <ThemeProvider theme={theme}>
+      <NavBarProvider>
+        <Component {...pageProps} />
+      </NavBarProvider>
+
+    </ThemeProvider>
+  </>
 }
-export default MyApp
