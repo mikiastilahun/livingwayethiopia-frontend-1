f2026a636b8cca17fdee4d818ddb1d3e1550489b
abeltyr <abeltyr@gmail.com>
2022-01-10 19:48:18 +0300

    (feat) support us update

--- src/sections/support-us/giving/index.tsx
+++ src/sections/support-us/giving/index.tsx
@@ -29,11 +29,11 @@ const GivingComponent = () => {
             <p className='text-center text-lg md:text-xl mt-10'>
                 Giving is an act of worship that indicates what we value most. When we give to the church, we partner with God to see changes happen and lives transformed, both here and abroad.
             </p>
-            <div className="sm:flex flex-wrap justify-center items-center text-center gap-8 mt-5">
+            <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
                 {data.loop.map((data: any, index) => {
-                    return <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4  mt-6  shadow-lg rounded-lg " style={{ background: `${theme.colors.primary}55` }}>
-                        <div className="flex-shrink-0">
-                            <div className="flex relative rou items-center mx-auto justify-center h-12 w-12 rounded-md ">
+                    return <div className="shadow-md rounded-2xl w-80 p-4 " style={{ background: `${theme.colors.background}` }}>
+                        <div className="flex flex-row items-start gap-4">
+                            <div className='relative rounded-md overflow-hidden' style={{ width: 100, height: 75 }} >
                                 <ImageContainer
                                     loading='lazy'
                                     placeholder="blur"
@@ -44,14 +44,19 @@ const GivingComponent = () => {
                                     src="https://images.unsplash.com/photo-1569437061233-5befcf96ee45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                                 />
                             </div>
+                            <div className=" w-full flex flex-col justify-between">
+                                <div>
+                                    <p className=" text-xl font-medium">
+                                        Bank Name
+                                    </p>
+                                    <p className="text-gray-400 text-xs">
+                                        Bank Details Lorem ipsum dolor sit amet, consec Lorem ipsum dolor sit amet, consec Lorem ipsum dolor
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <h3 className="text-2xl sm:text-xl text-white font-semibold  my-2">
-                            Bank Name
-                        </h3>
-                        <p className="text-sm text-white ">
-                            Bank related detail's
-                        </p>
                     </div>
+
                 })}
             </div>
         </div>
